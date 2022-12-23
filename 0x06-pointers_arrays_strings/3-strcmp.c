dd2212d1e6ffa24cffe2c45d0c48cc7c9b40e737
AbelHake <erdunoabel47@gmail.com>
2022-12-23 13:37:45 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,12 +18,12 @@ int _strcmp(char *s1, char *s2)
 		{
 	                if (s1[i] == '\0' && s2[i] == '\0')		
 				break;
-			else if (s1[i] == '\0'
+			else if (s1[i] == '\0')
 			{
 			        diff = s2[i];
 			        break;
 		        }
-		        else if (s2[i] == '\0'0
+		        else if (s2[i] == '\0')
 			{
 		 	        diff = s1[i];
 				break;
