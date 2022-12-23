1607e2361a6f6c7ba570bfbf67f723b0383e220e
AbelHake <erdunoabel47@gmail.com>
2022-12-23 12:29:07 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,7 +14,6 @@
        char *_strncat(char *dest, char *src, int n);
        char *_strncpy(char *dest, char *src, int n);
        char *string_toupper(char *);
-       char cap_string(char *);
        char *cap_string(char *);
        char *leet(char *);
        char *rot13(char *);
