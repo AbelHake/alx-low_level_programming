d581e1c9c19ac07445bbb0fb1b9ed1f131dcd14c
AbelHake <erdunoabel47@gmail.com>
2023-01-03 10:14:43 -0800

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,18 +3,17 @@
 /**
  * _strpbrk - prints the cosecutive caracters of s1 that are in s2.
  * @s: source string
- * @accept:
- * searching string
+ * @accept: searching string
  *
  * Return: new string.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i , j;
+	unsigned int i, j;
 
 	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; *(accept = j); j++)
+		for (j = 0; *(accept + j); j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
